fe1ec34a1e20ef4ab9dd111e28b5c5b8a41fd08a
werronense <31091965+werronense@users.noreply.github.com>
2019-07-03 06:43:50 -0400

    Add screen and simulated pixels

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  function resetScreen(dimension) {
-    
+  const screen = document.querySelector("#screen");
+
+  function generatePixels(dimension) {
+    for (i = 0; i < dimension; i++) {
+      const newRow = document.createElement("div");
+
+      newRow.classList.add("row");
+
+      for (j = 0; j < dimension; j++) {
+        const newPixel = document.createElement("div");
+        newPixel.classList.add("pixel");
+        newRow.appendChild(newPixel);
+      }
+
+      screen.appendChild(newRow);
+    }
   }
+  /*
+  function setScreen(selectedScreen) {
+
+  }
+
+  function resetScreen(selectedScreen) {
+
+  }
+  */
+  generatePixels(16);
 })
